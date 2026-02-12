95efa4877d28b7df7af7f7be8db53539ee8228bb
yoopiyo <77734588+githublaure@users.noreply.github.com>
2026-02-12 20:35:46 +0000

    fixed syntax

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -431,7 +431,7 @@ export default function Dashboard() {
                 )}
               </div>
             </div>
-            )}
+            </> )}
           </div>
         </div>
       </div>
